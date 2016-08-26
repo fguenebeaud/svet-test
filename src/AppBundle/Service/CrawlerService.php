ad687666d6b16e6d4ec6e1eff92c664cc92f8a60
Florent <florent.guenebeaud@gmail.com>
2016-08-26 07:54:55 +0200

    [ADD] - Ajout du service + début du dev du service

--- src/AppBundle/Service/CrawlerService.php
+++ src/AppBundle/Service/CrawlerService.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Service;
 
 use Goutte\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
 /**
  * Class CrawlerService
@@ -43,11 +44,28 @@ class CrawlerService
 
             $clientUrl = str_replace('[CATEGORY]', $category, $baseUrl);
             $clientUrl = str_replace('[PLACE]', $place, $baseUrl);
-          //  $clientUrl = str_replace('[PAGE]', $pageStart, $baseUrl);
-
 
             $client = new Client();
 
+            $cptAdvert = 0;
+            $pageStart = $clientArray['page_start'];
+
+            while (100 >= $cptAdvert) {
+                $clientUrl = str_replace('[PAGE]', $pageStart, $baseUrl);
+
+                //Appel de la requête
+                $crawler = $client->request($method, $clientUrl);
+                // Filtrage du contenu html, en ne récupérant que les sections (annonces)
+                $crawler->filter('section.item_infos')->each(function (Crawler $node) {
+                    
+                    $itemContent = $node->html();
+                    if (strpos($itemContent, 'item_price')) {
+                        echo $node->filter('.item_price')->text();
+                    } else {
+                        echo 'none';
+                    }
+                });
+            }
             $crawler = $client->request($method, $clientUrl);
         } else {
             throw new \Exception('La configuration pour '.$clientName.' n\'éxiste pas. Avez-configurer ce domaine ?');
